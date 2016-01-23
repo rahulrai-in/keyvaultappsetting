a22939e2c039f2ce437b1aefc51537f6e598705a
moonytheloony <rahulrai@live.com>
2016-01-23 12:09:48 +0530

    Added all untracked files.
    All code moved in

---
+++ KeyVaultAppSettingWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KeyVaultAppSettingWebApp.Startup))]
+namespace KeyVaultAppSettingWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
