a6572d12fe5c8d27bbd9132fce1618c1b32838f6
moonytheloony <rahulrai@live.com>
2016-01-23 12:07:09 +0530

    Moving in all changes

---
+++ KeyVaultAppSettingWebApp/SettingsProcessor.cs
@@ -0,0 +1,122 @@
+﻿// ***********************************************************************
+// Assembly         : KeyVaultAppSettingWebApp
+// Author           : rahulrai
+// Created          : 01-18-2016
+//
+// Last Modified By : rahulrai
+// Last Modified On : 01-18-2016
+// ***********************************************************************
+// <copyright file="SettingsProcessor.cs" company="">
+//     Copyright ©  2016
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+#region
+
+using System.Web;
+
+using KeyVaultAppSettingWebApp;
+
+#endregion
+
+[assembly: PreApplicationStartMethod(typeof(SettingsProcessor), "Start")]
+
+namespace KeyVaultAppSettingWebApp
+{
+    #region
+
+    using System;
+    using System.Configuration;
+    using System.Reflection;
+
+    using KeyVaultConfigurationHandler;
+
+    #endregion
+
+    /// <summary>
+    ///     Class SettingsProcessor.
+    /// </summary>
+    public static class SettingsProcessor
+    {
+        #region Constants
+
+        /// <summary>
+        ///     The application setting prefix
+        /// </summary>
+        private const string AppSettingPrefix = "APPSETTING-";
+
+        /// <summary>
+        ///     The SQL server prefix
+        /// </summary>
+        private const string SqlServerPrefix = "SQLCONNSTR-";
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     Starts this instance.
+        /// </summary>
+        public static void Start()
+        {
+            var keys = KeyVaultHandler.GetKeys();
+            foreach (var entry in keys)
+            {
+                var name = entry.Value;
+                var val = KeyVaultHandler.GetValue(entry.Key);
+
+                if (name.StartsWith(SqlServerPrefix, StringComparison.OrdinalIgnoreCase))
+                {
+                    name = name.Substring(SqlServerPrefix.Length);
+                    SetConnectionString(name, val, "System.Data.SqlClient");
+                }
+                else if (name.StartsWith(AppSettingPrefix, StringComparison.OrdinalIgnoreCase))
+                {
+                    //// Update AppSettings with new value.
+                    name = name.Substring(AppSettingPrefix.Length);
+                    ConfigurationManager.AppSettings[name] = val;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sets the connection string.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="connString">The connection string.</param>
+        /// <param name="providerName">Name of the provider.</param>
+        private static void SetConnectionString(string name, string connString, string providerName = null)
+        {
+            var settings = ConfigurationManager.ConnectionStrings[name];
+            settings?.SetData(connString, providerName);
+        }
+
+        /// <summary>
+        /// Sets the data.
+        /// </summary>
+        /// <param name="settings">The settings.</param>
+        /// <param name="connString">The connection string.</param>
+        /// <param name="providerName">Name of the provider.</param>
+        private static void SetData(this ConnectionStringSettings settings, string connString, string providerName)
+        {
+            var readOnlyField = typeof(ConfigurationElement).GetField(
+                "_bReadOnly",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+
+            readOnlyField.SetValue(settings, false);
+            settings.ConnectionString = connString;
+
+            if (providerName != null)
+            {
+                settings.ProviderName = providerName;
+            }
+        }
+
+        #endregion
+    }
+}
--- KeyVaultConfigurationHandler/KeyVaultHandler.cs
+++ KeyVaultConfigurationHandler/KeyVaultHandler.cs
@@ -1,22 +1,115 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ***********************************************************************
+// Assembly         : KeyVaultConfigurationHandler
+// Author           : rahulrai
+// Created          : 01-14-2016
+//
+// Last Modified By : rahulrai
+// Last Modified On : 01-18-2016
+// ***********************************************************************
+// <copyright file="KeyVaultHandler.cs" company="">
+//     Copyright ©  2016
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
 
 namespace KeyVaultConfigurationHandler
 {
-    using System.Configuration;
-    using System.Xml;
+    #region
 
-    public class KeyVaultHandler : IConfigurationSectionHandler
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.Azure.KeyVault;
+    using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+    #endregion
+
+    /// <summary>
+    /// Class KeyVaultHandler.
+    /// </summary>
+    public static class KeyVaultHandler
     {
-        public object Create(object parent, object configContext, XmlNode section)
+        #region Static Fields
+
+        /// <summary>
+        /// The vault name
+        /// </summary>
+        private static readonly string VaultName = ConfigurationManager.AppSettings["VaultName"];
+
+        /// <summary>
+        /// The kv client
+        /// </summary>
+        private static readonly KeyVaultClient KvClient = new KeyVaultClient(GetToken);
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Gets the keys.
+        /// </summary>
+        /// <returns>Dictionary&lt;System.String, System.String&gt;.</returns>
+        public static Dictionary<string, string> GetKeys()
         {
-            XmlNode newNode = section.OwnerDocument.CreateNode(XmlNodeType.Element, "appSettings", section.NamespaceURI);
-            // create an instance of the original handler and call it's create method returning the result.
-            NameValueFileSectionHandler handler = new NameValueFileSectionHandler();
-            return handler.Create(parent, configContext, newNode);
+            try
+            {
+                var result = KvClient.GetSecretsAsync(VaultName).Result;
+                return result.Value.ToDictionary(value => value.Id, value => value.Identifier.Name);
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>System.String.</returns>
+        public static string GetValue(string key)
+        {
+            try
+            {
+                return KvClient.GetSecretAsync(key).Result.Value;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets the token.
+        /// </summary>
+        /// <param name="authority">The authority.</param>
+        /// <param name="resource">The resource.</param>
+        /// <param name="scope">The scope.</param>
+        /// <returns>Task&lt;System.String&gt;.</returns>
+        /// <exception cref="System.InvalidOperationException">No Key Vault token</exception>
+        private static async Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authenticationContext = new AuthenticationContext(authority);
+            ////Alternatively, use certificate authentication
+            var clientCredential = new ClientCredential(
+                ConfigurationManager.AppSettings["ClientID"],
+                ConfigurationManager.AppSettings["ClientKey"]);
+            var result = await authenticationContext.AcquireTokenAsync(resource, clientCredential);
+            if (result == null)
+            {
+                throw new InvalidOperationException("No Key Vault token");
+            }
+
+            return result.AccessToken;
+        }
+
+        #endregion
     }
 }
